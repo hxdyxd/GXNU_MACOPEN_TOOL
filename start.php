12224fc52d4756ca29ba0d54700641fff8ddc0f7
hxdyxd <hxdyxd@gmail.com>
2016-10-17 16:23:09 +0800

    delete passwd

--- start.php
+++ start.php
@@ -15,6 +15,7 @@ use \Workerman\Worker;
 use \Workerman\WebServer;
 require_once './Workerman/Autoloader.php';
 
+date_default_timezone_set("Asia/Shanghai");
 $worker = new Worker('http://0.0.0.0:80');
 $worker->count = 4;
 $worker->name = 'Webpage-Process';
@@ -24,7 +25,7 @@ $worker_s->name = 'Send-Process';
 
 //the first create sql table
 //$db = new SQLite3('gxnu_macopen_tool.db');
-//$create_sql = 'CREATE TABLE MACOPEN (name varchar, mac varchar, isp int, passwd varchar, info varchar)';
+//$create_sql = 'CREATE TABLE MACOPEN (name varchar, mac varchar, isp int, info varchar)';
 //$db->exec($create_sql);
 
 $worker_s->onWorkerStart = function($worker_s){
@@ -85,14 +86,15 @@ $worker->onMessage = function($connection, $http){
 			$err = '2:MUST SUBMIT NAME.';
 		}
 	}else if(strpos($http['server']['REQUEST_URI'], 'Insert') && $http['server']['REQUEST_METHOD'] == 'POST'){
-		if(is_data($http['post']['name']) && is_data($http['post']['mac']) && is_data($http['post']['isp']) && is_data($http['post']['passwd'])){
+		if(is_data($http['post']['mac']) && is_data($http['post']['isp'])){
+			$name = 'GXNU.'.date("Y.M."). $connection->getRemoteIp().'.RECORD';
 			$is_recover = false;
 			$sql = "SELECT name,mac FROM MACOPEN";
 			$ret = $db->query($sql);
 			while($row = $ret->fetchArray(SQLITE3_ASSOC)){
-				if($row['name'] == $http['post']['name']){
+				if($row['name'] == $name){
 					$is_recover = true;
-					$err = '3:名字已经存在';
+					$err = '3:添加限制，请删除旧的记录或者更换网络环境后重试';
 					break;
 				}else if($row['mac'] == $http['post']['mac']){
 					$is_recover = true;
@@ -101,50 +103,48 @@ $worker->onMessage = function($connection, $http){
 				}
 			}
 			if(!$is_recover){
-				$name = $http['post']['name'];
 				$mac = $http['post']['mac'];
 				$isp = intval($http['post']['isp']);
-				$passwd = md5($http['post']['passwd'].$admin_passwd);
 				$info = json_decode(@file_get_contents('http://www.imfirewall.com/ip-mac-lookup/get_mac_info.php?mac=' . $mac), true);
 				if($info && $info['success'] == true){
 					$info = $info['result']['mac_producer'];
 				}else{
 					$info = 'NO-INFO';
 				}
-				$sql = "INSERT INTO MACOPEN VALUES ('$name', '$mac', $isp, '$passwd', '$info')";
+				$sql = "INSERT INTO MACOPEN (name, mac, isp, info) VALUES ('$name', '$mac', $isp, '$info')";
 				if( $db->exec($sql) ){
-					echo "ADD ". $http['post']['name']. " from ".$http['server']['REMOTE_ADDR']."\n";
-					$connection->send(json_encode(array('success'=>'1')));
+					echo "ADD ".$name."\n";
+					$connection->send(json_encode(array('success'=>'1', 'name'=>$name)));
 					return $connection->close();
 				}
 			}
 		}else{
-			$err = '6:只能输入字母和数字还有 :  -  . 哦';
+			$err = '6:只能输入字母和数字还有 : 哦';
 		}
 	}else if(strpos($http['server']['REQUEST_URI'], 'Delete') && $http['server']['REQUEST_METHOD'] == 'POST'){
-		if(is_data($http['post']['name']) && is_data($http['post']['passwd'])){
+		if(is_data($http['post']['name']) && is_data($http['post']['mac'])){
+			$name = $http['post']['name'];
 			$is_del = false;
-			$sql = "SELECT name,passwd FROM MACOPEN";
+			$sql = "SELECT name,mac FROM MACOPEN";
 			$ret = $db->query($sql);
 			while($row = $ret->fetchArray(SQLITE3_ASSOC)){
-				if($row['name'] == $http['post']['name'] && ($row['passwd'] == md5($http['post']['passwd'].$admin_passwd) || $http['post']['passwd'] == $admin_passwd)){
+				if($row['name'] == $name && ($row['mac'] == $http['post']['mac'] || $http['post']['mac'] == $admin_passwd)){
 					$is_del = true;
 					break;
 				}
 			}
 			if( $is_del ){
-				$name = $http['post']['name'];
 				$sql = "DELETE FROM MACOPEN WHERE name = '$name' ";
 				if( $db->exec($sql) ){
-					echo "DEL ". $http['post']['name']. " from ".$http['server']['REMOTE_ADDR']."\n";
+					echo "DEL ". $name."\n";
 					$connection->send(json_encode(array('success'=>'1')));
 					return $connection->close();
 				}
 			}else{
-				$err = '5:删除密匙错误';
+				$err = '5:物理地址输入错误';
 			}
 		}else{
-			$err = '6:只能输入字母和数字还有 :  -  . 哦';
+			$err = '6:只能输入字母和数字还有 : 哦';
 		}
 	}
 	$connection->send(json_encode( array('success'=>'0', 'msg'=> $err)));
@@ -155,7 +155,7 @@ $worker->onMessage = function($connection, $http){
  must len<20 && [0-9] [a-z] [A-Z] [- . :]
 */
 function is_data($data){
-	if( isset($data) && strlen($data)<=20 ){
+	if( isset($data) && strlen($data)<=50 ){
 		for($i=0; $i<strlen($data); $i++){
 			if( !(ord($data[$i]) >= 45 && ord($data[$i]) <= 58 && ord($data[$i]) != 47) && !(ord($data[$i]) >= 65 && ord($data[$i]) <= 90) && !(ord($data[$i]) >= 97 && ord($data[$i]) <= 122) ) return false;
 		}
